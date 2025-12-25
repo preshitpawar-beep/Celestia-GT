e5c33d11f0699d0a37ad84e6b4ae75e3d5a1944d
preshitpawar-beep <preshitpawar@gmail.com>
2025-12-25 17:58:48 +0000

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,1 +1,25 @@
 
+"use client";
+import Hero from "../components/Hero";
+import Products from "../components/Products";
+import QualityHero from "../components/QualityHero";
+import QualityCommitment from "../components/QualityCommitment";
+import Inspection from "../components/Inspection";
+import Documentation from "../components/Documentation";
+import Capabilities from "../components/Capabilities";
+import CTA from "../components/CTA";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Products />
+      <QualityHero />
+      <QualityCommitment />
+      <Inspection />
+      <Documentation />
+      <Capabilities />
+      <CTA />
+    </main>
+  );
+}
