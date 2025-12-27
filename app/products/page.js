3fec71ce64f31f7e8e0debae2ca0a03649f18a1d
preshitpawar-beep <preshitpawar@gmail.com>
2025-12-27 14:40:39 +0000

    Update page.js

--- app/products/page.js
+++ app/products/page.js
@@ -110,7 +110,6 @@ export default function ProductsPage() {
                     ))}
                   </ul>
 
-                  </div>
                 </div>
               </div>
             </motion.div>
